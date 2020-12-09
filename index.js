67a9d6dabc923262f0ec658ed82025f4f4953df2
ElayadeIsmail <elayadeismail@gmail.com>
2020-12-09 12:01:04 +0100

    make it better

--- index.js
+++ index.js
@@ -30,15 +30,16 @@ app.use('/api/users', require('./routes/auth'));
 app.use('/api/posts', require('./routes/posts'));
 
 // Error middleware
-app.use(errorMiddleware);
 
 if (process.env.NODE_ENV === 'production') {
-  app.use('/static', express.static(path.join(__dirname, 'client/build')));
+  app.use(express.static('client/build'));
 
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
+
+app.use(errorMiddleware);
 
 const PORT = process.env.PORT || 5000;
 
