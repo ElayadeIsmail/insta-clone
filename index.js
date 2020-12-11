0fe3bd31382c6b2c8c05ace4df5882573a661760
ElayadeIsmail <elayadeismail@gmail.com>
2020-12-11 18:52:37 +0100

    make it better

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 require('dotenv').config();
 const cookieSession = require('cookie-session');
 const cors = require('cors');
-const helmet = require('helmet');
+// const helmet = require('helmet');
 const compression = require('compression');
 const path = require('path');
 
@@ -14,7 +14,7 @@ const app = express();
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cors());
-app.use(helmet());
+// app.use(helmet());
 app.use(compression());
 app.use(
   cookieSession({
